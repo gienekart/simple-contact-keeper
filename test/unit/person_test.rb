c2ef45d51239ccaccfbfaf352bf893b878092ad3
Wojciech Rakszawski <gienek.art@wp.pl>
2009-08-23 14:55:09 +0200

    przetestowanie walidacji osoby

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -5,4 +5,20 @@ class PersonTest < ActiveSupport::TestCase
   def test_truth
     assert true
   end
+
+  def test_a_full_person_valid
+    @person = Person.find(people(:one).id)
+    assert @person.save
+  end
+
+  def test_a_not_full_person_valid
+    @person = Person.find(people(:two).id)
+    assert @person.save
+  end
+
+  def test_a_not_full_person_not_valid
+    @person = Person.find(people(:tree).id)
+    assert_equal( @person.save, false)
+  end
+  
 end
