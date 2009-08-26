d67535d0cc33ab576d665aa0123e14fa6ff4285d
Wojciech Rakszawski <gienek.art@wp.pl>
2009-08-26 21:39:48 +0200

    dodano dane kontaktowe do bazy

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090823122116) do
+ActiveRecord::Schema.define(:version => 20090826192423) do
+
+  create_table "contacts", :force => true do |t|
+    t.integer "person_id"
+    t.string  "data"
+  end
 
   create_table "people", :force => true do |t|
     t.string "name"
--- test/unit/contact_test.rb
+++ test/unit/contact_test.rb
@@ -5,4 +5,19 @@ class ContactTest < ActiveSupport::TestCase
   def test_truth
     assert true
   end
+
+  def test_a_full_contact_valid
+    @contact = Contact.find(people(:one).id)
+    assert @contact.save
+  end
+
+  def test_a_not_full_contact_not_valid
+    @contact = Contact.find(people(:two).id)
+    assert_equal( @contact.save, false)
+  end
+
+  def test_a_vrong_person_contact_not_valid
+    @contact = Contact.find(people(:tree).id)
+    assert_equal( @contact.save, false)
+  end
 end
